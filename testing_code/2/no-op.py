58abefe2185494e0789577d1931bb9affd476dcd
taras-svystun <tatarik.sv@gmail.com>
2022-06-06 15:07:17 +0300

    finished 2 chapter

---
+++ testing_code/2/no-op.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest import result
+
+if '__builtins__' not in dir() or not hasattr(__builtins__, 'profile'):
+    def profile(func):
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+        return inner
+
+@profile
+def random_function(param):
+    return param ** 2
+
+class TestCase(unittest.TestCase):
+    def test(seld):
+        result = random_function(5)
+        self.assertEquals(result, 25)
